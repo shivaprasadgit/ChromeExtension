0ddbf0084bd8498d0d9045c18d0839c34a2fc35f
shivaprasadgit <shivaprasad.ds@outlook.com>
2019-10-30 14:41:41 +0530

    first commit

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
-    var newURL = "http://www.google.com/";
+    var newURL = "https://www.google.com/search?q=video&oq=video&aqs=chrome..69i57j0l2j69i61l3.2519j0j7&sourceid=chrome&ie=UTF-8/";
     chrome.tabs.create({ url: newURL });
   });
